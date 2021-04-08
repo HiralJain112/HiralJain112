9bcea8741f645c85c32ab8c15d87ff05b9bd7c0c
Hiral Jain <71397276+HiralJain112@users.noreply.github.com>
2021-04-08 23:54:46 +0530

    Add files via upload

---
+++ string_function.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+   /* string str;
+    string s1="fam",s2="ily";
+   */
+   /* s1=s1+s2;
+    cout<<s1;
+    */
+    //cout<<s1[1];
+    //clear fn
+    /*string abc="bhujbhkbh  iuyjkhnkhjk       uyuihjk";
+    abc.clear();
+    cout<<abc;
+     */
+    //Comparing two strings
+   /* string s1="abc";
+    string s2="abc";
+    cout<<s2.compare(s1)<<endl;
+    */
+   //empty
+  /* string s1="";
+   if (s1.empty())
+   {
+       cout<<"s1 is empty";
+   }
+   */
+  //erase 
+  /* string s1="nincompoop";
+   s1.erase(3,2);
+   cout<<s1;
+    */
+   //find
+   /*string s1="nincompoop";
+   cout<<s1.find("poo")<<endl;
+   */
+  //insert
+  /*string s1="nincompoop";
+  s1.insert(2,"lol");
+  cout<<s1<<endl;
+  //length
+  cout<<s1.length()<<" "<<s1.size();
+
+  cout<<s1.substr(6,4);
+  */
+ string s1="786";
+ int x=stoi(s1);
+ cout<<x;
+ cout<<to_string(x)+"2";
+   return 0;
+}
