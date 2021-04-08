ac2338af7875cf751d3141572b8e2ca17d918a83
Hiral Jain <71397276+HiralJain112@users.noreply.github.com>
2021-04-08 23:54:22 +0530

    Add files via upload

---
+++ strings.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+    string str;
+    //string str1(5,'n');
+   cin>>str;
+   // cout<<str1<<endl;
+   /*string str2="ApniMasti";
+   cout<<str2;*/
+   //getline(cin,str);
+   cout<<str<<endl;
+    return 0;
+
+}
