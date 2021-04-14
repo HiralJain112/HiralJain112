5d24f17e6ec348870a51a37bad94cb8c4e26a284
Hiral Jain <71397276+HiralJain112@users.noreply.github.com>
2021-04-14 22:32:13 +0530

    Add files via upload

---
+++ 2Darray.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int m,n,k;
+    cin>>m>>n>>k;
+    int arr[m][n];
+    for (int i = 0; i < m; i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            cin>>arr[i][j];
+        }
+    }
+    for (int i = 0; i < m; i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            if (arr[i][j]==k)
+            {
+                cout<<i<<" "<<j;
+            }
+            
+        }
+      
+    }
+    return 0;
+}
