3f4fdd7f80b321340776e4adb74ed7d8b01d947b
Hiral Jain <71397276+HiralJain112@users.noreply.github.com>
2021-04-17 23:06:54 +0530

    Add files via upload

---
+++ check_palindrome.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    char arr[n+1];
+    cin>>arr;
+    bool check=1;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i]!=arr[n-i-1])
+        {
+            check=0;
+            break;
+        }        
+    }
+    if (check==true)
+    {
+        cout<<"is a palindrome"<<endl;
+    }
+    else
+    {
+        cout<<"is not a palindrome";
+    }
+    
+    return 0;
+}
